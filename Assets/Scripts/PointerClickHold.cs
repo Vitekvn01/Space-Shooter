06d518cdf2fc6dc12729e26707c2f9368c8ee215
vitekvn01 <vitekvn01@yandex.ru>
2024-04-01 12:21:28 +0700

    Добавлен скрипт отслеживающий нажатие

---
+++ Assets/Scripts/PointerClickHold.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Common;
+
+namespace Common
+{
+    public class PointerClickHold : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+    {
+        private bool _hold;
+        public bool IsHold => _hold;
+        void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+        {
+            _hold = true;
+        }
+
+        void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+        {
+            _hold = false;
+        }
+    }
+
+}
+
